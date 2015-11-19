f3a70b7cd636354a665f54107b8215e31bb83fde
shuang5 <shuang@renci.org>
2015-11-19 15:54:02 -0500

    move choicenet to a different package.

--- src/main/java/choiceNet/Metric.java
+++
@@ -1,6 +0,0 @@
-package choiceNet;
-
-public interface Metric {
-	//returns 1 if greater than m, 0 if equal, -1 if less
-	int compareTo(Metric m);
-}
--- src/main/java/choiceNet/Predicate.java
+++
@@ -1,5 +0,0 @@
-package choiceNet;
-
-public interface Predicate<Input,Output> {
-	boolean test(Service<Input,Output> s1, Service<Input,Output> s2);
-}
